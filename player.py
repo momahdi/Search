33d3246ef32d17d08de2c88ee870c5ed3b56687f
momahdi <mohamed4mahdi@gmail.com>
2022-11-24 18:47:28 +0100

    10 points on kattis

--- player.py
+++ player.py
@@ -4,6 +4,7 @@ import random
 from fishing_game_core.game_tree import Node
 from fishing_game_core.player_utils import PlayerController
 from fishing_game_core.shared import ACTION_TO_STR
+import math
 
 
 class PlayerControllerHuman(PlayerController):
@@ -51,19 +52,64 @@ class PlayerControllerMinimax(PlayerController):
             self.sender({"action": best_move, "search_time": None})
 
     def search_best_next_move(self, initial_tree_node):
-        """
-        Use minimax (and extensions) to find best possible next move for player 0 (green boat)
-        :param initial_tree_node: Initial game tree node
-        :type initial_tree_node: game_tree.Node
-            (see the Node class in game_tree.py for more information!)
-        :return: either "stay", "left", "right", "up" or "down"
-        :rtype: str
-        """
 
         # EDIT THIS METHOD TO RETURN BEST NEXT POSSIBLE MODE USING MINIMAX ###
 
         # NOTE: Don't forget to initialize the children of the current node
         #       with its compute_and_get_children() method!
 
-        random_move = random.randrange(5)
-        return ACTION_TO_STR[random_move]
+        children = initial_tree_node.compute_and_get_children()
+        calculated_move = 0
+        min_score = -math.inf
+
+
+        for child in children:
+            calculated_score = self.alphabeta(child , 2 ,-math.inf, math.inf,0 )
+            if(min_score < calculated_score):
+               min_score = calculated_score
+               calculated_move = child.move
+        return ACTION_TO_STR[calculated_move]
+
+    def alphabeta(self, node, depth, alpha, beta, maximizingPlayer):
+            if depth == 0 or list(node.state.get_fish_positions().keys()) == []:
+                return self.calculate_heuristics(node, 0)
+
+
+            else:
+                if (maximizingPlayer == 0):
+                    Value = -math.inf
+                    for child in node.compute_and_get_children() :
+                        Value = max(Value, self.alphabeta(child, depth-1, alpha, beta, 1))
+                        alpha = max(alpha, Value)
+                        if (beta <= alpha ): 
+                            break # β cutoff
+
+                else :
+                    Value = math.inf
+                    for child in node.compute_and_get_children() :
+                        Value = min(Value,  self.alphabeta(child, depth-1, alpha, beta, 0))
+                        beta = min(beta, Value)
+                        if (beta <= alpha):
+                            break # α cutoff 
+
+                return Value
+
+    def calculate_heuristics(self, node, player):
+        state = node.state
+        max_score, min_score = state.get_player_scores()
+        heuristic = max_score - min_score
+        
+        hook_postion= state.hook_positions[player]
+        fish_postions= state.fish_positions.values()
+        
+        heuristic = heuristic - self.get_dist_to_closest_fish(hook_postion,fish_postions)
+        return heuristic
+
+    def get_dist_to_closest_fish(self, hook_postion, fish_postions):
+        distance = math.inf
+        for fish_postion in fish_postions:
+            distance = min(distance, self.get_dist_to_fish(hook_postion, fish_postion))
+        return distance
+
+    def get_dist_to_fish(self, hook_postion, fish_position):
+        return math.sqrt((fish_position[0]-hook_postion[0])**2 + (fish_position[1]-hook_postion[1])**2)
